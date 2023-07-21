b561df7eaed6a17abd0bf5ddd71b965b77efad72
Seliaste <aena.aria@protonmail.com>
2023-07-21 22:37:18 +0200

    added .config

---
+++ .config/cmus/lib.pl
@@ -0,0 +1,130 @@
+/mnt/HDD/Music/seliaste unreleased/seliaste - chor.wav
+/mnt/HDD/Music/seliaste unreleased/seliaste - synth3.wav
+/mnt/HDD/Music/[LLC06] 5ubaruu & saves - S5 (2022)/01. 5ubaruu - Tired Eyes (Intro).flac
+/mnt/HDD/Music/[LLC06] 5ubaruu & saves - S5 (2022)/02. 5ubaruu - Parisian Skyline.flac
+/mnt/HDD/Music/[LLC06] 5ubaruu & saves - S5 (2022)/03. 5ubaruu & Saves - Contemplation.flac
+/mnt/HDD/Music/[LLC06] 5ubaruu & saves - S5 (2022)/04. 5ubaruu & Saves - Lead Filled Lungs.flac
+/mnt/HDD/Music/[LLC06] 5ubaruu & saves - S5 (2022)/05. Saves - Finches.flac
+/mnt/HDD/Music/[LLC06] 5ubaruu & saves - S5 (2022)/06. Saves - Soul Scales.flac
+/mnt/HDD/Music/[LLC06] 5ubaruu & saves - S5 (2022)/07. 5ubaruu - Moving On.flac
+/mnt/HDD/Music/AFX 1991 Analogue Bubblebath - EP/01 Analogue Bubblebath.flac
+/mnt/HDD/Music/1. soul doubt.flac
+/mnt/HDD/Music/bjork - homogenic 01 - hunter.flac
+/mnt/HDD/Music/Trilogy/01 Escape From Midwich Valley.flac
+/mnt/HDD/Music/Trilogy/02 Disco Zombi Italia.flac
+/mnt/HDD/Music/Trilogy/03 L.A. Venice Bitch 80's.flac
+/mnt/HDD/Music/Trilogy/04 Wake Up the President.flac
+/mnt/HDD/Music/Trilogy/05 347 Midnight Demons.flac
+/mnt/HDD/Music/Trilogy/06 Le Perv.flac
+/mnt/HDD/Music/Trilogy/07 Roller Mobster.flac
+/mnt/HDD/Music/Trilogy/08 Meet Matt Stryker.flac
+/mnt/HDD/Music/Trilogy/09 Obituary.flac
+/mnt/HDD/Music/Trilogy/10 Looking for Tracy Tzu.flac
+/mnt/HDD/Music/Trilogy/11 Sexkiller on the Loose.flac
+/mnt/HDD/Music/Trilogy/12 Hang'em All.flac
+/mnt/HDD/Music/Trilogy/13 Division Ruine.flac
+/mnt/HDD/Music/Trilogy/14 Paradise Warfare.flac
+/mnt/HDD/Music/Trilogy/15 Run, Sally, Run!.flac
+/mnt/HDD/Music/Trilogy/16 Turbo Killer.flac
+/mnt/HDD/Music/Trilogy/17 Anarchy Road.flac
+/mnt/HDD/Music/Trilogy/18 Invasion A.D_.flac
+/mnt/HDD/Music/Furi Original Soundtrack/01 Time to Wake Up.flac
+/mnt/HDD/Music/Furi Original Soundtrack/02 Danger 6_24.flac
+/mnt/HDD/Music/Furi Original Soundtrack/03 Enraged.flac
+/mnt/HDD/Music/Furi Original Soundtrack/04 A Picture in Motion.flac
+/mnt/HDD/Music/Furi Original Soundtrack/05 Wisdom of Rage.flac
+/mnt/HDD/Music/Furi Original Soundtrack/06 Unraveled.flac
+/mnt/HDD/Music/Furi Original Soundtrack/07 Set Me Free.flac
+/mnt/HDD/Music/Furi Original Soundtrack/08 What We Fight For.flac
+/mnt/HDD/Music/Furi Original Soundtrack/09 Make This Right.flac
+/mnt/HDD/Music/Furi Original Soundtrack/10 Make This Right (remix).flac
+/mnt/HDD/Music/Furi Original Soundtrack/11 Love and Madness.flac
+/mnt/HDD/Music/Furi Original Soundtrack/12 You're Mine.flac
+/mnt/HDD/Music/Furi Original Soundtrack/12 You’re Mine.flac
+/mnt/HDD/Music/Furi Original Soundtrack/13 A Big Day.flac
+/mnt/HDD/Music/Furi Original Soundtrack/14 Something Memorable.flac
+/mnt/HDD/Music/Furi Original Soundtrack/15 A Monster.flac
+/mnt/HDD/Music/Furi Original Soundtrack/16 You Are the End.flac
+/mnt/HDD/Music/Furi Original Soundtrack/17 My Only Chance.flac
+/mnt/HDD/Music/Furi Original Soundtrack/18 Shambles.flac
+/mnt/HDD/Music/Furi Original Soundtrack/19 Danger 7_53.flac
+/mnt/HDD/Music/Furi Original Soundtrack/20 Danger 8_02.flac
+/mnt/HDD/Music/Furi Original Soundtrack/21 Danger 19_06.flac
+/mnt/HDD/Music/Furi Original Soundtrack/22 Danger 19_07.flac
+/mnt/HDD/Music/Hakushi Hasegawa - Somoku Hodo/01 - 草木 (Somoku).flac
+/mnt/HDD/Music/Hakushi Hasegawa - Somoku Hodo/02 - 毒 (Doku).flac
+/mnt/HDD/Music/Hakushi Hasegawa - Somoku Hodo/03 - 它会消失 (Ta hui xiaoxi).flac
+/mnt/HDD/Music/Hakushi Hasegawa - Somoku Hodo/04 - 妾薄命 (Qie boming).flac
+/mnt/HDD/Music/Hakushi Hasegawa - Somoku Hodo/05 - キュー (Que).flac
+/mnt/HDD/Music/Hakushi Hasegawa - Somoku Hodo/06 - はみ出す指 (Hamidasu Yubi).flac
+/mnt/HDD/Music/2019 - First Blue Sky [FLAC]/Placid Angles - First Blue Sky - 01 First Blue Sky.flac
+/mnt/HDD/Music/2019 - First Blue Sky [FLAC]/Placid Angles - First Blue Sky - 02 Angel.flac
+/mnt/HDD/Music/2019 - First Blue Sky [FLAC]/Placid Angles - First Blue Sky - 03 A Moment Away From You.flac
+/mnt/HDD/Music/2019 - First Blue Sky [FLAC]/Placid Angles - First Blue Sky - 04 Vent.flac
+/mnt/HDD/Music/2019 - First Blue Sky [FLAC]/Placid Angles - First Blue Sky - 05 Earth and Everything.flac
+/mnt/HDD/Music/2019 - First Blue Sky [FLAC]/Placid Angles - First Blue Sky - 06 Ocean Floor.flac
+/mnt/HDD/Music/2019 - First Blue Sky [FLAC]/Placid Angles - First Blue Sky - 07 Bad Minds.flac
+/mnt/HDD/Music/2019 - First Blue Sky [FLAC]/Placid Angles - First Blue Sky - 08 1700.flac
+/mnt/HDD/Music/2019 - First Blue Sky [FLAC]/Placid Angles - First Blue Sky - 09 Breathe Her In.flac
+/mnt/HDD/Music/2019 - First Blue Sky [FLAC]/Placid Angles - First Blue Sky - 10 Soft Summer (Revisited).flac
+/mnt/HDD/Music/07 - Placid Angles - Dakota.flac
+/mnt/HDD/Music/Rezzett - Rezzett LP (2018) {The Trilogy Tapes - TTT070} [WEB FLAC]/07 - Rezzett - Gremlinz.flac
+/mnt/HDD/Music/Rezzett - Rezzett LP (2018) {The Trilogy Tapes - TTT070} [WEB FLAC]/01 - Rezzett - Hala.flac
+/mnt/HDD/Music/Rezzett - Rezzett LP (2018) {The Trilogy Tapes - TTT070} [WEB FLAC]/02 - Rezzett - Longboat.flac
+/mnt/HDD/Music/Rezzett - Rezzett LP (2018) {The Trilogy Tapes - TTT070} [WEB FLAC]/03 - Rezzett - Sexzzy Creep.flac
+/mnt/HDD/Music/Rezzett - Rezzett LP (2018) {The Trilogy Tapes - TTT070} [WEB FLAC]/06 - Rezzett - Tarang.flac
+/mnt/HDD/Music/Rezzett - Rezzett LP (2018) {The Trilogy Tapes - TTT070} [WEB FLAC]/05 - Rezzett - Wet Bilge.flac
+/mnt/HDD/Music/Rezzett - Rezzett LP (2018) {The Trilogy Tapes - TTT070} [WEB FLAC]/08 - Rezzett - Worst Ever Contender.flac
+/mnt/HDD/Music/Rezzett - Rezzett LP (2018) {The Trilogy Tapes - TTT070} [WEB FLAC]/04 - Rezzett - Yunus in Ekstasi.flac
+/mnt/HDD/Music/Forgot About Her/1-01 So Cold in Koreatown.mp3
+/mnt/HDD/Music/Forgot About Her/1-02 Petty Theft.mp3
+/mnt/HDD/Music/Forgot About Her/1-03 Forgot About Her.mp3
+/mnt/HDD/Music/Forgot About Her/1-04 Cold Shoulder.mp3
+/mnt/HDD/Music/Forgot About Her/1-05 Bent Mode.mp3
+/mnt/HDD/Music/Forgot About Her/1-06 Drunk on Petroleum.mp3
+/mnt/HDD/Music/Forgot About Her/1-07 Graffiti Blues.mp3
+/mnt/HDD/Music/Forgot About Her/1-08 Shoulda Stayed Home.mp3
+/mnt/HDD/Music/Forgot About Her/1-09 Skate Drink Cry.mp3
+/mnt/HDD/Music/Forgot About Her/1-10 Just Over U.mp3
+/mnt/HDD/Music/Forgot About Her/1-11 Throw It Away.mp3
+/mnt/HDD/Music/Forgot About Her/1-12 Bus Token.mp3
+/mnt/HDD/Music/Forgot About Her/1-13 Saw U Last Nite.mp3
+/mnt/HDD/Music/Forgot About Her/1-14 From Da Wasteland.mp3
+/mnt/HDD/Music/Forgot About Her/1-15 Get Over It.mp3
+/mnt/HDD/Music/Forgot About Her/1-16 Saguaro.mp3
+/mnt/HDD/Music/Forgot About Her/1-17 Cold Shoulder (feat. Brrd) (Rem.mp3
+/mnt/HDD/Music/Forgot About Her/1-18 So Cold in Koreatown (feat. Brr.mp3
+/mnt/HDD/Music/Soichi Terada & Masalo - Diving Into Minds (Club Mix).flac
+/mnt/HDD/Music/Rare songs I like/DJacuzzi - Running From You.flac
+/mnt/HDD/Music/windowshopping - ethereal plane (large body of water)/01 - windowshopping - empty promise.flac
+/mnt/HDD/Music/windowshopping - ethereal plane (large body of water)/02 - windowshopping - day of night 3.flac
+/mnt/HDD/Music/windowshopping - ethereal plane (large body of water)/03 - windowshopping - pale.flac
+/mnt/HDD/Music/windowshopping - ethereal plane (large body of water)/04 - windowshopping - go to hell max.flac
+/mnt/HDD/Music/windowshopping - ethereal plane (large body of water)/05 - windowshopping - no prospects.flac
+/mnt/HDD/Music/windowshopping - ethereal plane (large body of water)/06 - windowshopping - stop remembering.flac
+/mnt/HDD/Music/windowshopping - ethereal plane (large body of water)/07 - windowshopping - epilogue feat. 5ubaruu.flac
+/mnt/HDD/Music/windowshopping - ethereal plane (large body of water)/08 - windowshopping - imitatrice.flac
+/mnt/HDD/Music/windowshopping - ethereal plane (large body of water)/09 - windowshopping - église feat. rainsdeaf.flac
+/mnt/HDD/Music/Andromeda/Windowshopping - Andromeda - 01 kg3166.flac
+/mnt/HDD/Music/Andromeda/Windowshopping - Andromeda - 02 Heaven (Wish U Were Here Mix).flac
+/mnt/HDD/Music/Andromeda/Windowshopping - Andromeda - 03 Girl156BPM.flac
+/mnt/HDD/Music/Andromeda/Windowshopping - Andromeda - 04 Amethyst Sex Magick.flac
+/mnt/HDD/Music/Andromeda/Windowshopping - Andromeda - 05 Lucydrop Typefuck.flac
+/mnt/HDD/Music/Andromeda/Windowshopping - Andromeda - 06 Baby Angel Typhoid Mary.flac
+/mnt/HDD/Music/Andromeda/Windowshopping - Andromeda - 07 Angel Dust Pneumonic Maru.flac
+/mnt/HDD/Music/Andromeda/Windowshopping - Andromeda - 08 4344334.flac
+/mnt/HDD/Music/Andromeda/Windowshopping - Andromeda - 09 Nier Synopsis (No Prospects 2nd Mix).flac
+/mnt/HDD/Music/Andromeda/Windowshopping - Andromeda - 10 Danbooru DMT Phantasy.flac
+/mnt/HDD/Music/Andromeda/Windowshopping - Andromeda - 11 Weightless Death, Homesick Venutian.flac
+/mnt/HDD/Music/Andromeda/Windowshopping - Andromeda - 12 Sunderland's Revenge.flac
+/mnt/HDD/Music/Andromeda/Windowshopping - Andromeda - 13 Synopsis Circling My Head.flac
+/mnt/HDD/Music/Hakushi Hasegawa - Air Ni Ni/01. あなただけ (Only You).flac
+/mnt/HDD/Music/Hakushi Hasegawa - Air Ni Ni/02. ｏ (＿＿_).flac
+/mnt/HDD/Music/Hakushi Hasegawa - Air Ni Ni/03. 怖いところ (Scary point).flac
+/mnt/HDD/Music/Hakushi Hasegawa - Air Ni Ni/04. 砂漠で (Desert).flac
+/mnt/HDD/Music/Hakushi Hasegawa - Air Ni Ni/05. 風邪山羊 (Cold Goat).flac
+/mnt/HDD/Music/Hakushi Hasegawa - Air Ni Ni/06. 蕊のパーティ (Stamens, Pistils, Parties).flac
+/mnt/HDD/Music/Hakushi Hasegawa - Air Ni Ni/07. 悪魔 (Evil Things).flac
+/mnt/HDD/Music/Hakushi Hasegawa - Air Ni Ni/08. いつくしい日々 (Itsukushii Hibi).flac
+/mnt/HDD/Music/Hakushi Hasegawa - Air Ni Ni/09. 山が見える (I can See Mountains).flac
+/mnt/HDD/Music/Hakushi Hasegawa - Air Ni Ni/10. ニュートラル (Neutral).flac
